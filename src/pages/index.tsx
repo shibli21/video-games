241049af48aea0f7bd64760d1b6cea71e570a139
shibli21 <syedshiblimahmud@gmail.com>
2020-11-25 12:26:41 +0600

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Container, Heading } from "@chakra-ui/react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <Container maxW="xl">
+      <Heading>Games</Heading>
+    </Container>
+  );
+}
